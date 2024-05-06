39cb2f3f8a11252413c97c1b23b6284dff156454
alokehpdev <alok.regmi@ehp.dev>
2024-05-06 18:42:33 +0545

    Don't use corfu for now

--- init.el
+++ init.el
@@ -21,6 +21,9 @@
 
        :completion
        company           ; the ultimate code completion backend
+       ;; (corfu
+       ;;  +icons
+       ;;  +orderless)
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
