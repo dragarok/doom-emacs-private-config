bd086ea17d9ad95b3268d57f9ef65c1fccea4ff7
alokehpdev <alok.regmi@ehp.dev>
2024-05-06 21:02:01 +0545

    Remove unused packages

--- packages.el
+++ packages.el
@@ -6,18 +6,18 @@
 (package! org-web-tools)
 ;; Testing
 ;; analyze when you have entered data and filter activity based on your clocking
-(package! org-analyzer)
+;; (package! org-analyzer)
 (package! org-mru-clock) ;; clocking in and out easier
 (package! grab-x-link) ;; grabbing links from firefox, chrome and so on.
-(package! xkcd)
-(package! anki-editor)
+;; (package! xkcd)
+;; (package! anki-editor)
 (package! nepali-romanized
   :recipe (:host github :repo "bishesh/emacs-nepali-romanized"))
 (package! org-clock-convenience)
 (package! magit-delta
   :recipe (:host github :repo "dandavison/magit-delta"))
-(package! ov
-  :recipe (:host github :repo "emacsorphanage/ov.el"))
+;; (package! ov
+;;   :recipe (:host github :repo "emacsorphanage/ov.el"))
 
 ;; Who likes hydra when it's not centered?
 (package! hydra-posframe
@@ -32,8 +32,8 @@
 
 ;; handle very large files
 (package! vlf :recipe (:host github :repo "m00natic/vlfi" :files ("*.el")))
-(package! ox-ipynb
-  :recipe (:host github :repo "jkitchin/ox-ipynb"))
+;; (package! ox-ipynb
+;;   :recipe (:host github :repo "jkitchin/ox-ipynb"))
 ;; easy to read auto fill without doing anything
 (package! virtual-auto-fill)
 (package! org-fragtog)
@@ -49,10 +49,10 @@
   :recipe (:host github
            :repo "tecosaur/org-pandoc-import"
            :files ("*.el" "filters" "preprocessors")))
-(package! wallabag
-  :recipe (:host github
-           :repo "chenyanming/wallabag.el"
-           :files ("*.el" "*.alist" "*.css")))
+;; (package! wallabag
+;;   :recipe (:host github
+;;            :repo "chenyanming/wallabag.el"
+;;            :files ("*.el" "*.alist" "*.css")))
 (package! page-break-lines)
 (package! websocket)
 (package! org-roam-ui :recipe (:host github :repo "org-roam/org-roam-ui" :files ("*.el" "out")))
@@ -64,10 +64,10 @@
 (package! org-appear)
 (package! copilot
   :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")))
-(package! elfeed-tube
-  :recipe (:host github :repo "karthink/elfeed-tube"))
-(package! elfeed-tube-mpv
-  :recipe (:host github :repo "karthink/elfeed-tube"))
+;; (package! elfeed-tube
+;;   :recipe (:host github :repo "karthink/elfeed-tube"))
+;; (package! elfeed-tube-mpv
+;;   :recipe (:host github :repo "karthink/elfeed-tube"))
 (package! numpydoc
   :recipe (:host github :repo "douglasdavis/numpydoc.el"))
 ;; (package! evil-colemak-basics)
@@ -189,7 +189,7 @@
 
 ;; (package! dockerfile-mode)
 ;; (package! docker)
-(package! elfeed-score)
+;; (package! elfeed-score)
 (unpin! emacsql)
 (package! org-books
   :recipe (:host github :repo "goderich/org-books"))
@@ -201,11 +201,11 @@
 ;;   :recipe (:host github :repo "orzechowskid/tsi.el"))
 ;;; (pa! org-time-budgets
 ;;   :recipe (:host github :repo "leoc/org-time-budgets" :branch "develop" :no-byte-compile t)) ;; budgeting on tasks
-(package! org-transclusion)
-(package! org-remark)
+;; (package! org-transclusion)
+;; (package! org-remark)
 ;; (unpin! ox-hugo)
 ;; (package! gptel)
-(package! org-fancy-priorities)
+;; (package! org-fancy-priorities)
 
 ;; (unpin! saveplace-pdf-view)
 
