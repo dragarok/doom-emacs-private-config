c2a734fe639ac6a5a5e3e53e91dbba597f549aed
alokehpdev <alok.regmi@ehp.dev>
2026-01-04 16:08:43 +0545

    Make it clear

--- config.el
+++ config.el
@@ -185,13 +185,6 @@
 
 (setq! citar-notes-paths '(org-roam-directory))
 
-;; Prevent citar-org-roam from triggering org-roam-db-sync (prevents slow rebuilds)
-;; We temporarily make org-roam-db-sync a no-op during citar-org-roam-setup
-(defadvice! my/citar-org-roam-setup-no-sync (fn &rest args)
-  :around #'citar-org-roam-setup
-  (cl-letf (((symbol-function 'org-roam-db-sync) #'ignore))
-    (apply fn args)))
-
 ;; Common derived paths (work on both platforms)
 (setq org-logs-directory (concat org-directory "logs/"))
 (setq org-agenda-directory (concat org-directory "agenda/"))
@@ -243,6 +236,15 @@
   (require 'prodigy-services)      ; dev server management
   (require 'jupyter-config)        ; Jupyter/EIN notebook support
   (require 'browser-bookmarks))    ; Chrome/Brave bookmark utilities
+
+
+;; Prevent citar-org-roam from triggering org-roam-db-sync (prevents slow rebuilds)
+;; We temporarily make org-roam-db-sync a no-op during citar-org-roam-setup
+(if IS-ANDROID
+    (defadvice! my/citar-org-roam-setup-no-sync (fn &rest args)
+      :around #'citar-org-roam-setup
+      (cl-letf (((symbol-function 'org-roam-db-sync) #'ignore))
+        (apply fn args))))
 
 ;; ============================================================
 ;; GENERAL SETTINGS
