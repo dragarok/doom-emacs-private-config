d834a3fe55fa2dcc45f713778d07b0007d2132b3
alokehpdev <alok.regmi@ehp.dev>
2026-01-09 20:54:34 +0545

    Fix consult recent file fn

--- config.el
+++ config.el
@@ -636,7 +636,7 @@ _p_rev       _u_pper              _=_: upper/lower       _r_esolve
   (interactive)
   (find-file
    (consult--read
-    (or (message (mapcar #'abbreviate-file-name recentf-list))
+    (or (mapcar #'abbreviate-file-name recentf-list)
         (user-error "No recent files, `recentf-mode' is %s"
                     (if recentf-mode "on" "off")))
     :prompt "Find recent file: "
