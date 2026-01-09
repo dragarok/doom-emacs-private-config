3efa37ad0b4d0ad72cfe0f156b55d9096f8d1f12
alokehpdev <alok.regmi@ehp.dev>
2026-01-09 20:49:11 +0545

    Minor updates

--- lisp/productivity_addons.el
+++ lisp/productivity_addons.el
@@ -9,7 +9,9 @@
   "Open the most recently downloaded file from Downloads folder.
 Uses dired/dirvish to handle file opening automatically."
   (interactive)
-  (let* ((downloads-dir (expand-file-name "~/Downloads/"))
+  (let* ((downloads-dir (if (eq system-type 'android)
+                            "/sdcard/Download/"
+                          (expand-file-name "~/Downloads/")))
          (files (directory-files downloads-dir t "^[^.]" t))
          (latest-file (car (sort files
                                  (lambda (a b)
@@ -20,7 +22,7 @@ Uses dired/dirvish to handle file opening automatically."
           (dired downloads-dir)
           (dired-goto-file latest-file)
           (dired-find-file))
-      (message "No files found in Downloads folder"))))
+      (message "No files found in Downloads folder: %s" downloads-dir))))
 
 (defun org-roam-dailies-goto-monday-of-week ()
   "Find the daily-note for the Monday of the current week, creating it if necessary."
