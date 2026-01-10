4d25aa6a2611af02e61c4181f985274174ed0e9e
alokehpdev <alok.regmi@ehp.dev>
2026-01-10 07:15:57 +0545

    File relative name

--- lisp/android-extras.el
+++ lisp/android-extras.el
@@ -138,7 +138,11 @@ Stores in `org-directory`/attachments/captured-images/YYYY-MM-DD/."
                  (description (if (string-empty-p caption)
                                   datetime-str
                                 (format "%s %s" datetime-str caption)))
-                 (relative-path (file-relative-name dest-file (file-name-directory (buffer-file-name)))))
+                 ;; Use file-truename to resolve symlinks (e.g. ~/org vs /sdcard/org)
+                 ;; ensuring clean relative paths like ../attachments/...
+                 (relative-path (file-relative-name 
+                                 (file-truename dest-file) 
+                                 (file-truename (file-name-directory (buffer-file-name))))))
             
             (unless (file-exists-p dest-dir)
               (make-directory dest-dir t))
