587c43e0f14990f807f44db7a1ec400388562632
alokehpdev <alok.regmi@ehp.dev>
2026-02-20 14:16:13 +0545

    Add texlive to path for emacs android

--- init.el
+++ init.el
@@ -215,10 +215,11 @@
 ;; Termux PATH setup for Android
 (when IS-ANDROID
   (let ((termux-bin "/data/data/com.termux/files/usr/bin")
-        (home-bin "/data/data/com.termux/files/home/bin"))
-    ;; Prepend ~/bin and Termux bin to PATH
-    (setenv "PATH" (concat home-bin ":" termux-bin ":" (getenv "PATH")))
-    (setq exec-path (cons home-bin (cons termux-bin exec-path)))
+        (home-bin "/data/data/com.termux/files/home/bin")
+        (texlive-bin "/data/data/com.termux/files/usr/bin/texlive"))
+    ;; Prepend ~/bin, texlive, and Termux bin to PATH
+    (setenv "PATH" (concat home-bin ":" texlive-bin ":" termux-bin ":" (getenv "PATH")))
+    (setq exec-path (cons home-bin (cons texlive-bin (cons termux-bin exec-path))))
 
     ;; Optional: explicitly force ssh program (extra safety)
     (let ((termux-ssh (concat termux-bin "/ssh")))
